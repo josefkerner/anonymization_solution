716692dd2a1991de34a9e998a9516015afd52395
josefkerner <kerner.jo@gmail.com>
2024-10-29 15:39:23 +0100

    init

--- README.md
+++ README.md
@@ -9,12 +9,17 @@ This solution parses and anonymizes source documents for RAG AI use cases
 pip install -r requirements.txt
 ```
 4. place the source documents in the `data` folder
-5. export the env var PYTHONPATH in the root of the project
+5. change location to root of the project
 ```bash
-set PYTHONPATH=.
+cd anonymization_solution
 ```
-6. Run the following command to start the application:
+6. export the env var PYTHONPATH in the root of the project
+```bash
+set PYTHONPATH=. 
+or export PYTHONPATH=. (for linux and mac users)
+```
+7. Run the following command to start the application:
 ```bash
 python src/main.py
 ```
-7. The anonymized documents will be saved in the `results` folder in txt format
+8. The anonymized documents will be saved in the `results` folder in txt format
--- src/anonymization/anonymizer.py
+++ src/anonymization/anonymizer.py
@@ -1,13 +1,13 @@
-from presidio_analyzer import AnalyzerEngine
-from presidio_anonymizer import AnonymizerEngine
+
+import re
+import unidecode
 from src.language.language_detector import LinguaLanguageDetector
 from anonymization import (Anonymization, AnonymizerChain,
                            PhoneNumberAnonymizer,
                            EmailAnonymizer, NamedEntitiesAnonymizer)
 class Anonymizer:
     def __init__(self):
-        self.analyzer = AnalyzerEngine()
-        self.anonymizer = AnonymizerEngine()
+
         self.language_detector = LinguaLanguageDetector()
 
 
@@ -19,11 +19,34 @@ class Anonymizer:
         if lang == 'cs':
             lang = 'cs_CZ'
             ner_model = 'en_core_web_lg'
-
-
+        elif lang == 'de':
+            lang = 'de_DE'
+            ner_model = 'de_core_news_lg'
         elif lang == 'en':
             lang = 'en_US'
             ner_model = 'en_core_web_lg'
+        else:
+            raise ValueError(f"Unsupported language: {lang}")
+
+        #remove diacritics
+        #text = unidecode.unidecode(text)
+        #replace every number in text with 'X'
+        text = ''.join(['0' if c.isdigit() else c for c in text])
+
+        #replace every company name followed by 'a.s' with 'COMPANY a.s.'
+        #company name can contain diacritics such as Česká spořitelna a.s.
+        pattern = r'([áéíóúůýžčřďťňÁÉÍÓÚŮÝŽČŘĎŤŇ]|[a-z]|[A-Z])+ a\.s\.'
+        text = re.sub(pattern, 'COMPANY', text)
+
+        #replace every company name followed by 's.r.o' with 'COMPANY s.r.o.'
+        text = re.sub(r'([áéíóúůýžčřďťňÁÉÍÓÚŮÝŽČŘĎŤŇ]|[a-z]|[A-Z])+ s\.r\.o\.', 'COMPANY s.r.o.', text)
+
+        #replace every company name followed by 'gmbh' with 'COMPANY s.r.o.'
+        text = re.sub(r'([A-Z][a-z]+ )+gmbh', 'COMPANY gmbh', text)
+
+
+
+
         #exit(1)
         self.anon = AnonymizerChain(Anonymization(lang))
         self.anon.add_anonymizers(EmailAnonymizer,
